e65e526d816873a8643c443d83fc1265fce1e900
Jacob Adlers <jacob.adlers@gmail.com>
2023-11-21 03:24:36 +0100

    Add possibility to score a match

--- src/routes/[id]/[gameId]/+page.server.ts
+++ src/routes/[id]/[gameId]/+page.server.ts
@@ -1,5 +1,6 @@
 import prisma from '$lib/prisma';
-import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ params: { id, gameId } }) => {
 	const game = await prisma.game.findFirstOrThrow({
@@ -13,3 +14,72 @@ export const load = (async ({ params: { id, gameId } }) => {
 	const team2 = game.team2.map((pid) => players.find((p) => pid === p.id));
 	return { game, team1, team2 };
 }) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ request, params: { gameId } }) => {
+		const data = await request.formData();
+
+		// Remove previous score from user, if first time adding will remove 0
+		const game = await prisma.game.findFirstOrThrow({
+			where: { id: gameId }
+		});
+
+		const score1str = data.get('score1');
+		const score2str = data.get('score2');
+		if (!score1str || typeof score1str !== 'string' || !score2str || typeof score2str !== 'string')
+			return fail(400, { error: 'missing required score' });
+		const score1 = parseInt(score1str);
+		const score2 = parseInt(score2str);
+
+		// reset if points are already given
+		if (game.score1 !== 0 && game.score2 !== 0) {
+			await Promise.all([
+				...game.team1.map((playerId) =>
+					prisma.player.update({
+						where: { id: playerId },
+						data: {
+							points: { decrement: game.score1 ?? 0 },
+							numberOfGames: { decrement: 1 }
+						}
+					})
+				),
+				...game.team2.map((playerId) =>
+					prisma.player.update({
+						where: { id: playerId },
+						data: {
+							points: { decrement: game.score2 ?? 0 },
+							numberOfGames: { decrement: 1 }
+						}
+					})
+				)
+			]);
+		}
+
+		// Update game and players
+		await prisma.game.update({
+			where: { id: gameId },
+			data: { score1, score2 }
+		});
+
+		await Promise.all([
+			...game.team1.map((playerId) =>
+				prisma.player.update({
+					where: { id: playerId },
+					data: {
+						points: { increment: score1 ?? 0 },
+						numberOfGames: { increment: 1 }
+					}
+				})
+			),
+			...game.team2.map((playerId) =>
+				prisma.player.update({
+					where: { id: playerId },
+					data: {
+						points: { increment: score2 ?? 0 },
+						numberOfGames: { increment: 1 }
+					}
+				})
+			)
+		]);
+	}
+} satisfies Actions;
--- src/routes/[id]/[gameId]/+page.svelte
+++ src/routes/[id]/[gameId]/+page.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
+	import { teamMembers } from '$lib/util';
 	export let data: PageLoad;
 </script>
 
 <h1>Match {data.game.id}</h1>
+
+<form method="POST">
+	<label>
+		<input name="score1" type="number" min="0" />
+		{teamMembers(data.team1)}
+	</label>
+	<br />
+	vs.
+	<br />
+	<label>
+		<input name="score2" type="number" min="0" />
+		{teamMembers(data.team2)}
+	</label>
+	<br />
+	<input type="submit" value="skicka in" />
+</form>
