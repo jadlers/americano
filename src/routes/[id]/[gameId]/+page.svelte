d34b8420b11c24a3e117f2096ed71dad631fb2aa
Jacob Adlers <jacob.adlers@gmail.com>
2023-11-21 04:17:09 +0100

    fixup! Show current score when scoring a game

--- src/routes/[id]/[gameId]/+page.svelte
+++ src/routes/[id]/[gameId]/+page.svelte
@@ -7,14 +7,24 @@
 
 <form method="POST">
 	<label>
-		<input name="score1" type="number" min="0" value={data.game.score1} />
+		<input
+			name="score1"
+			type="number"
+			min="0"
+			value={data.game.score1 === 0 ? '' : data.game.score1}
+		/>
 		{teamMembers(data.team1)}
 	</label>
 	<br />
 	vs.
 	<br />
 	<label>
-		<input name="score2" type="number" min="0" value={data.game.score2} />
+		<input
+			name="score2"
+			type="number"
+			min="0"
+			value={data.game.score2 === 0 ? '' : data.game.score2}
+		/>
 		{teamMembers(data.team2)}
 	</label>
 	<br />
