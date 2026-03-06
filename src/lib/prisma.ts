6e5ada10fa34c63e44d5ec56aa7ce093d17b450f
Jacob Adlers <jacob.adlers@gmail.com>
2026-03-06 21:57:18 +0100

    Update prisma to v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { DATABASE_URL } from '$env/static/private';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL
-});
-
-const prisma = new PrismaClient({
-	adapter
-});
+const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
